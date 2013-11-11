377efc6971b0af3233847fc93f518eb9e0f07bce
Aaron Swensen <aaron.swensen@gmail.com>
2013-11-11 09:21:09 -0700

    Added more text replacements from http://xkcd.com/1288/

--- Source/content_script.js
+++ Source/content_script.js
@@ -35,6 +35,29 @@ function handleText(textNode)
 	v = v.replace(/\bThe cloud\b/g, "My butt");
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bwitnesses\b/g, "these dudes I know");
+	v = v.replace(/\ballegedly\b/g, "kinda probably");
+	v = v.replace(/\bnew study\b/g, "Tumblr post");
+	v = v.replace(/\brebuild\b/g, "avenge");
+	v = v.replace(/\bspace\b/g, "spaaace");
+	v = v.replace(/\bGoogle Glass\b/g, "Virtual Boy");
+	v = v.replace(/\bsmartphone\b/g, "Pokedex");
+	v = v.replace(/\belectric\b/g, "atomic");
+	v = v.replace(/\bElectric\b/g, "Atomic");
+	v = v.replace(/\bSenator\b/g, "Elf-Lord");
+	v = v.replace(/\bSenators\b/g, "Elf-Lords");
+	v = v.replace(/\bsenator\b/g, "elf-lord");
+	v = v.replace(/\bsenators\b/g, "elf-lords");
+	v = v.replace(/\bcar\b/g, "cat");
+	v = v.replace(/\belection\b/g, "eating contest");
+	v = v.replace(/\bcongressional leaders\b/g, "river spirits");
+	v = v.replace(/\bCongressional leaders\b/g, "River spirits");
+	v = v.replace(/\bHomeland Security\b/g, "Homestar Runner");
+	v = v.replace(/\bcould not be reached for comment\b/g, "is guilty and everyone knows it");
+	
+	// a few more for good measure
+	v = v.replace(/\bsurveillance\b/g, "break dancing");
+	v = v.replace(/\bthe White House\b/g, "White Castle");
 	
 	textNode.nodeValue = v;
 }
